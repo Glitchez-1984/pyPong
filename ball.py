1aa8e74af65ba1ac5b8b7f40368c7e88a6459a79
Glitchez <87347098+David-Safro@users.noreply.github.com>
2022-10-13 20:49:24 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,10 +1,12 @@
 import pygame
 from controls import *
+import random
+import time
 
 
 class Ball:
-    MAX_VEL = 5
-    color = WHITE
+    MAX_VEL = BALL_SPEED
+    color = FG
 
     def __init__(self, x, y, radius):
         self.x = x
@@ -17,8 +19,9 @@ class Ball:
     def restart(self):
         self.x = WIDTH // 2
         self.y = HEIGHT // 2
-        self.y_vel = 0
+        self.y_vel = random.randint(-5, 5)
         self.x_vel = self.x_norm
+
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -26,11 +29,19 @@ class Ball:
         self.x += self.x_vel
         self.y += self.y_vel
 
-    def check_win(self, p1, p2):
+    def check_score(self, p1, p2):
+        name = None
+        win = False
         if self.x >= WIDTH:
             p1 += 1
             self.restart()
         elif self.x <= 0:
             p2 += 1
             self.restart()
-        return p1, p2
+        if p1 == 10:
+            win = True
+            name = "Player 1"
+        elif p2 == 10:
+            win = True
+            name = "Player 2"
+        return p1, p2, (win, name)
