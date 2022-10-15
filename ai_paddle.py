f5163e603aa86e4ffd49d66639eea084cc1c45fd
Glitchez <87347098+David-Safro@users.noreply.github.com>
2022-10-14 20:59:11 -0400

    Update ai_paddle.py

--- ai_paddle.py
+++ ai_paddle.py
@@ -1,8 +1,8 @@
 import pygame
 from controls import *
-import random
 
-class Paddle_ai:
+
+class AI_Paddle:
     def __init__(self, height, x, y, speed, color):
         self.height = height
         self.width = 15
@@ -16,21 +16,9 @@ class Paddle_ai:
         pygame.draw.rect(win, self.color, paddle_rect)
 
     def move(self, ball_y):
-
-        middle = self.y - (self.height//2)
-        y_dif = abs(ball_y - (self.y + self.height // 2))
-        if ball_y > self.y <= HEIGHT - self.height - 10 and not(self.y + self.speed > ball_y):
+        if ball_y > self.y and self.y <= HEIGHT - self.height - 10 and not (
+                self.y + self.speed > ball_y):
             self.y += self.speed
-        elif ball_y < self.y >= 10 and not(self.y - self.speed < ball_y):
+        elif ball_y < self.y and self.y >= 10 and not (
+                self.y - self.speed < ball_y):
             self.y -= self.speed
-        '''
-        if self.y < ball_y:
-            self.y += self.speed
-        if self.y + self.height > ball_y:
-            self.y -= self.speed
-
-        if self.y <= 0:
-           self.y = 0
-        if self.y + self.height >= HEIGHT:
-            self.y = HEIGHT - self.height
-        '''
